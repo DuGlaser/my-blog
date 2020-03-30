0cd1f2bf1582b4cc95448f88645743fc86c06814
DuGlaser <duglaser0618@gmail.com>
2020-03-30 22:10:13 +0900

    :fire:DEL header switch process

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,56 +6,13 @@ import { css } from "@emotion/core"
 import Header from "./header"
 
 const wrapper = css({
-  padding: "5%",
+  padding: "5% 10%",
 })
 
 const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  let header
-
-  if (location.pathname === rootPath) {
-    header = (
-      <h1
-        style={{
-          ...scale(1.5),
-          marginBottom: rhythm(1.5),
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <h3
-        style={{
-          fontFamily: `Montserrat, sans-serif`,
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h3>
-    )
-  }
   return (
     <>
-      <Header>{header}</Header>
+      <Header />
       <div css={wrapper}>
         <main>{children}</main>
         <footer>
