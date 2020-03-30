9eee304fd815ff65c2a9245b80798b268740ed65
DuGlaser <duglaser0618@gmail.com>
2020-03-30 21:00:16 +0900

    :recycle:FIX layout style

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 
 import { rhythm, scale } from "../utils/typography"
+import { css } from "@emotion/core"
+import Header from "./header"
+
+const wrapper = css({
+  padding: "5%",
+})
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -48,22 +54,15 @@ const Layout = ({ location, title, children }) => {
     )
   }
   return (
-    <div
-      style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(24),
-        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-      }}
-    >
-      <header>{header}</header>
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </footer>
-    </div>
+    <>
+      <Header>{header}</Header>
+      <div css={wrapper}>
+        <main>{children}</main>
+        <footer>
+          <small>© {new Date().getFullYear()}.damegane</small>
+        </footer>
+      </div>
+    </>
   )
 }
 
