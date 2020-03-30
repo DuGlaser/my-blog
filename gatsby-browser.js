33254beae03091fb320d97a53e9f874737421b69
DuGlaser <duglaser0618@gmail.com>
2020-03-30 20:59:49 +0900

    :recycle:FIX import file

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 // custom typefaces
-// import "typeface-montserrat"
-// import "typeface-merriweather"
-//
-// import "prismjs/themes/prism.css"
 import "./src/style/reset.css"
+import "typeface-montserrat"
+import "typeface-merriweather"
+
+import "prismjs/themes/prism.css"
